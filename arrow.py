c1025bef898a5e1e1b2abc37ab10213893e73f17
kilhyeongyeong <51154237+kilhyeongyeong@users.noreply.github.com>
2020-09-05 16:00:17 +0900

    Update arrow.py

--- arrow.py
+++ arrow.py
@@ -23,8 +23,6 @@ threading_Time = 0.01
 c_W = 320
 c_H = 240
 FPS = 90
-
-
 # ------------------모션 제어--------------------------- 
 def TX_data_py2(ser, one_byte):  # one_byte= 0~255
     """
@@ -70,13 +68,12 @@ def Receiving(ser):
                 receiving_exit = 0
                 break
 
+arrow=""
+Count_L = 0
+Count_R = 0
+y_left=0
+y_right=0
 
-# -------------라인찾기------------------------------
-
-# -------------미션---------------------------------
-# 해야됨
-# **************************************************
-# **************************************************
 if __name__ == '__main__':
     os_version = platform.platform()
     print(" ---> OS " + os_version)
@@ -106,17 +103,22 @@ if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
     cap.set(3, c_W)
     cap.set(4, c_H)
-
+    im_cnt = 0
     while True:
         # 영상 값 불러옴 img가 영상
         ret, img = cap.read()
         if not ret:
             break
+
         gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         corners = cv2.goodFeaturesToTrack(gray, 100, 0.01, 5, blockSize=3, useHarrisDetector=True, k=0.03)
 
+        ymax = 0
         for i in corners:
+            ymax = max(ymax, i[0, 1])
             cv2.circle(img, tuple(i[0]), 3, (0, 0, 255), 2)
+
+        cv2.imwrite('/home/pi/Desktop/run/' + 'im_{}.jpg'.format(im_cnt), img)
 
         cv2.imshow('Video Test', img)
         key = 0xFF & cv2.waitKey(1)
@@ -124,6 +126,42 @@ if __name__ == '__main__':
             cap.release()
             cv2.destroyAllWindows()
             break
+
+        for i in corners:
+            if i[0, 1] == ymax:
+                x = i[0, 0]
+
+        edge = cv2.Canny(gray, 50, 150)
+        cv2.imwrite('/home/pi/Desktop/run/' + 'Canny_{}.jpg'.format(im_cnt), edge)
+        im_cnt += 1
+
+        for i in range(0, 240):
+            if edge[i, int(x - 10)] != 0:
+                y_left = i
+            if edge[i, int(x + 10)] != 0:
+                y_right = i
+
+        if y_left > y_right:
+            print("L")
+            Count_L += 1
+        else:
+            print("R")
+            Count_R += 1
+
+        if Count_L == 3:
+            print("LEFT!")
+            arrow = "L"
+            Count_L = 0
+            Count_R = 0
+            break;
+        elif Count_R == 3:
+            print("RIGHT!")
+            arrow = "R"
+            Count_L = 0
+            Count_R = 0
+            break;
+        else:
+            continue
 
 
     # -----  remocon 16 Code  Exit ------
