b573d499e56056362bf1c3324724dd397b81be37
kilhyeongyeong <51154237+kilhyeongyeong@users.noreply.github.com>
2020-09-16 15:51:32 +0900

    Update arrow.py

--- arrow.py
+++ arrow.py
@@ -7,10 +7,7 @@ import cv2
 import serial
 import time
 import sys
-import pytesseract
 from threading import Thread
-
-pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract'
 
 serial_use = 1
 
@@ -23,7 +20,9 @@ threading_Time = 0.01
 c_W = 320
 c_H = 240
 FPS = 90
-# ------------------모션 제어--------------------------- 
+
+
+# ------------------모션 제어---------------------------
 def TX_data_py2(ser, one_byte):  # one_byte= 0~255
     """
         ser : 연결된 시리얼 번호
@@ -68,11 +67,12 @@ def Receiving(ser):
                 receiving_exit = 0
                 break
 
-arrow=""
+
 Count_L = 0
 Count_R = 0
-y_left=0
-y_right=0
+y_left = 0
+y_right = 0
+ymax = 0
 
 if __name__ == '__main__':
     os_version = platform.platform()
@@ -103,44 +103,46 @@ if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
     cap.set(3, c_W)
     cap.set(4, c_H)
-    im_cnt = 0
+
+    cnt = 0
     while True:
         # 영상 값 불러옴 img가 영상
         ret, img = cap.read()
         if not ret:
             break
+        img = cv2.medianBlur(img, 5)
 
-        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        corners = cv2.goodFeaturesToTrack(gray, 100, 0.01, 5, blockSize=3, useHarrisDetector=True, k=0.03)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        _, img_th = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY)
 
-        ymax = 0
-        for i in corners:
-            ymax = max(ymax, i[0, 1])
-            cv2.circle(img, tuple(i[0]), 3, (0, 0, 255), 2)
-
-        cv2.imwrite('/home/pi/Desktop/run/' + 'im_{}.jpg'.format(im_cnt), img)
-
-        cv2.imshow('Video Test', img)
+        cv2.imshow("threshold", img_th)
         key = 0xFF & cv2.waitKey(1)
         if key == 27:  # ESC  Key
             cap.release()
             cv2.destroyAllWindows()
             break
 
-        for i in corners:
-            if i[0, 1] == ymax:
-                x = i[0, 0]
+        for j in range(240):
+            for i in range(320):
+                if img_th[j, i] == 0:
+                    if ymax <= j:
+                        x = i
+                        ymax = j
 
-        edge = cv2.Canny(gray, 50, 150)
-        cv2.imwrite('/home/pi/Desktop/run/' + 'Canny_{}.jpg'.format(im_cnt), edge)
-        im_cnt += 1
-
-        for i in range(0, 240):
-            if edge[i, int(x - 10)] != 0:
-                y_left = i
-            if edge[i, int(x + 10)] != 0:
-                y_right = i
-
+        for i in range(240):
+            if x - 20 <= 0:
+                if img_th[i, 0] == 0:
+                    y_left = i
+            else:
+                if img_th[i, x - 20] == 0:
+                    y_left = i
+            if x + 20 >= 320:
+                if img_th[i, 319] == 0:
+                    y_right = i
+            else:
+                if img_th[i, x + 20] == 0:
+                    y_right = i
+                    
         if y_left > y_right:
             print("L")
             Count_L += 1
@@ -153,16 +155,21 @@ if __name__ == '__main__':
             arrow = "L"
             Count_L = 0
             Count_R = 0
+            ymax = 0
             break;
         elif Count_R == 3:
             print("RIGHT!")
             arrow = "R"
             Count_L = 0
             Count_R = 0
+            ymax = 0
             break;
         else:
+            ymax = 0
+            y_left = 0
+            y_right = 0
+            x = 0
             continue
-
 
     # -----  remocon 16 Code  Exit ------
     while receiving_exit == 1:
